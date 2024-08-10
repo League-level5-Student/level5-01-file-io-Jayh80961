ba5924407e07f91d2078046b8bb1a7ee14ca7693
Hide On Bush <59904920+Jayh80961@users.noreply.github.com>
2024-08-10 10:21:11 -0700

    Update DirectoryIterator.java

--- src/_04_Directory_Iteration/DirectoryIterator.java
+++ src/_04_Directory_Iteration/DirectoryIterator.java
@@ -1,33 +1,45 @@
 package _04_Directory_Iteration;
 
 import java.io.File;
-
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.JFileChooser;
 
 public class DirectoryIterator {
 	public static void main(String[] args) {
-		/* 
-		 * The following is an example of how to list all of the files in a directory.
-		 * Once the program is running, the directory is chosen using the JFileChooser.
-		 */
+		// Choose the src directory using JFileChooser
 		JFileChooser jfc = new JFileChooser();
-		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		int returnVal = jfc.showOpenDialog(null);
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
 			File directory = jfc.getSelectedFile();
-			File[] files = directory.listFiles();
-			if(files != null) {
-				for(File f : files) {
-				  System.out.println(f.getAbsolutePath());
+			addCopyrightToJavaFiles(directory);
+		}
+	}
+
+	public static void addCopyrightToJavaFiles(File directory) {
+		// Get all files and directories in the current directory
+		File[] files = directory.listFiles();
+		if (files != null) {
+			for (File f : files) {
+				if (f.isDirectory()) {
+					// If the file is a directory, recursively call this method
+					addCopyrightToJavaFiles(f);
+				} else if (f.getName().endsWith(".java")) {
+					// If the file is a .java file, add the copyright statement
+					addCopyright(f);
 				}
 			}
 		}
-		
-		/*
-		 * Your task is to write a program that iterates through the src folder of this current Java Project. 
-		 * For every .java file it finds, the program will add a (non-legally binding) copyright statement at the bottom.
-		 * Be aware of possible directories inside of directories.
-		 * (e.g //Copyright © 2019 FirstName LastName)
-		 */
+	}
+
+	public static void addCopyright(File javaFile) {
+		try (FileWriter fw = new FileWriter(javaFile, true)) {
+			// Add a new line and the copyright statement at the end of the file
+			fw.write("\n//Copyright © 2019 FirstName LastName\n");
+			System.out.println("Added copyright to " + javaFile.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
